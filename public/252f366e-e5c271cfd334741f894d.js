2154efd6290dddacae138856b6ef93e437cf4d25
hiro <hiroooon69@gmail.com>
2025-07-28 08:44:03 +0900

    feat: build for github pages

---
+++ public/252f366e-e5c271cfd334741f894d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[916],{9644:function(t,a,e){e.d(a,{pgR:function(){return h}});var c=e(4084);function h(t){return(0,c.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{fillRule:"evenodd",d:"M14 6V4h-4v2h4zM4 8v11h16V8H4zm16-2c1.11 0 2 .89 2 2v11c0 1.11-.89 2-2 2H4c-1.11 0-2-.89-2-2l.01-11c0-1.11.88-2 1.99-2h4V4c0-1.11.89-2 2-2h4c1.11 0 2 .89 2 2v2h4z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-e5c271cfd334741f894d.js.map
